ed0b33e761e21424555e05e766330f19cac11d39
lelilia <lelilia@posteo.de>
2025-12-02 05:57:34 +0100

    day 1 cleanup

--- day_1.py
+++ day_1.py
@@ -1,37 +1,53 @@
 # Advent of Code 2025 - day 1
 
-import re
+
 INPUT = "input1"
 # INPUT = "testinput1"
 
-with open(INPUT) as f:
-    data = f.readlines()
 
-counter = 0
-counter_pass = 0
-pos = 50
-for d in data:
-    direction, distance = d[0], int(d[1:])
-    if direction == "L":
+def part_1(data):
+    counter = 0
+    pos = 50
+    for d in data:
+        direction, distance = d[0], int(d[1:])
+        if direction == "L":
+            pos = (pos - distance) % 100
+        else:
+            pos = (pos + distance) % 100
         if pos == 0:
-            counter_pass -= 1
-        pos -= distance
-        while pos < 0:
-            counter_pass += 1
-            pos += 100
-    else:
-        pos += distance
-        while pos > 99:
-            counter_pass += 1
-            pos -= 100
+            counter += 1
+    return counter
+
+
+def part_2(data):
+    counter = 0
+    counter_pass = 0
+    pos = 50
+    for d in data:
+        direction, distance = d[0], int(d[1:])
+        if direction == "L":
+            if pos == 0:
+                counter_pass -= 1
+            pos -= distance
+            while pos < 0:
+                counter_pass += 1
+                pos += 100
+        else:
+            pos += distance
+            while pos > 99:
+                counter_pass += 1
+                pos -= 100
+            if pos == 0:
+                counter_pass -= 1
+
         if pos == 0:
-            counter_pass -= 1
-
-    if pos == 0:
-        counter += 1
-
-print("Part 1:", counter)
-print("Part 2:", counter + counter_pass)
+            counter += 1
+    return counter + counter_pass
 
 
+if __name__ == "__main__":
+    with open(INPUT) as f:
+        data = f.readlines()
 
+    print("Part 1:", part_1(data))
+    print("Part 2:", part_2(data))
